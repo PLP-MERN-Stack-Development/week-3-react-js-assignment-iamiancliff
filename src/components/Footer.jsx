1080819378fc5b54a835271406e74d31d99791a3
Ian Cliff <heisiancliff@gmail.com>
2025-06-25 13:54:45 +0300

    Add Footer.jsx with copyright

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+// Footer component with basic links
+function Footer() {
+  return (
+    <footer className="bg-blue-500 dark:bg-gray-800 p-4 text-white text-center">
+      <p>&copy; 2025 Task Manager. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
